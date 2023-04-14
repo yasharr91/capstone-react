e94cf914e25af17a7404ee2283b44a5ec6231813
yashar amrollahi <113164350+yasharr91@users.noreply.github.com>
2023-04-14 18:14:09 +0430

    router added

---
+++ src/HomePage.js
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <p>Homepage</p>
+    </div>
+  );
+}
--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,38 @@
+import BookingPage from "./Home/BookingPage";
+import HomePage from "./HomePage";
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
 const Nav = () => {
   return (
-    <ul>
-      <li>
-        <a href="index.html"></a>
-      </li>
-      <li>
-        <a href="index.html"></a>
-      </li>
-      <li>
-        <a href="index.html"></a>
-      </li>
-    </ul>
+    <>
+      <BrowserRouter>
+        <Link to="/"> Home </Link>
+        <Link to="/booking"> Booking </Link>
+        <Routes>
+           
+          <Route path="/" element={<HomePage />}>
+            {"hjhj "}
+            Homepage
+          </Route>
+          <Route path="/booking" element={<BookingPage />}>
+            Bookingpage
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      {/* <ul>
+        <li>
+          <a href="index.html">Home</a>
+        </li>
+        <li>
+          <a href="index.html"></a>
+        </li>
+        <li>
+          <a href="index.html"></a>
+        </li>
+      </ul> */}
+    </>
   );
 };
 export default Nav;
